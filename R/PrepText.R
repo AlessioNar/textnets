401d4cd7cc7bc443cdaaa0aef9d5e326595d8551
Alessio Nardin <alessio.nardin@gmail.com>
2020-10-13 10:39:30 +0200

    Preptext documentation updated

--- R/PrepText.R
+++ R/PrepText.R
@@ -1,17 +1,27 @@
-# This prep_text function reads in a dataframe and requires two variable names
-# one that describes the document name, and one that describes the text of
-# the document. The node_type argument describes whether the user wants to
-# create a network where the nodes are words within a document, or the nodes are the
-# documents themselves. If the input data are tweets rather than regular text, 
-# tokenizer should be specified as `tokenizer = "tweets"` which will correctly tokenize
-# hashtags and @-mentions, as well as allowing to remove urls using `strip_url = TRUE`. 
-# Additional arguments to be passed to tokenizer are `strip_punct` which defaults to TRUE
-# and `strip_numeric` defaulting to FALSE. The `udmodel_lang` argument provides the option 
-# to pass a preloaded udpipe model (created with udpipe_download_model and udpipe_load_model) 
-# to the function eliminating the need to redownload it with every call to the function.
-# The function creates a dataframe in summarized tidy text format (where each row of the dataset 
-# describes the prevalence of each word within the document). Optionally, the function a) parses noun compounds;
-# b) removes numeric tokens; c) removes stop words; and b) returns nouns and proper nouns only.
+#' @title Prepare text for analysis 
+#'
+#' @description  The function creates a dataframe in summarized tidy text format (where each row of the dataset describes the prevalence of each word within the document). 
+#'
+#' @param dataframe Dataframe
+#' @param textvar Name of the column containing the text
+#' @param groupvar Name of the column containing the grouping variable
+#' @param tokenizer Describes whether the input data are tweets rather than regular text. If tokenizer = "tweets" then hashtag and mentions will be tokenized correctly, as well as the url removed. 
+#' @param strip_punct Boolean. If true, it will automatically remove punctuation
+#' @param node_type Describes whether the user wants to create a network where the nodes are words within a document, or the nodes are the documents themselves.
+#' @param pos If pos = "nouns" it will returns only nouns or proper nouns.
+#' @param language 
+#' @param remove_stop_words Boolean. If true, it will automatically remove stopwords
+#' @param remove_numbers Boolean. If true, it will automatically remove all numbers
+#' @param compound_nouns Boolean. If true, it will parse noun compounds
+#' @param udmodel_lang It provides the possibility to pass a preloaded udpipe language model
+#' 
+#' 
+#' @return Dataframe summarized in tidytext format
+#'
+#' @examples \dontrun{PrepText(textdata, groupvar, textvar)}
+#'
+#' @export PrepText
+#' 
 
 PrepText <- function(textdata, groupvar, textvar, node_type = c("groups","words"), 
                     tokenizer = c("words", "tweets"), pos = c("all", "nouns"),
